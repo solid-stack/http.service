b1dca6395bf745c8422655291fa1b6df457ed1d3
Peter Ajtai <peter.ajtai@thinksolid.com>
2016-12-19 23:17:00 -0800

    Refactoring a little based on superagent promises

--- index.js
+++ index.js
@@ -1,112 +1,100 @@
 'use strict';
 const authLocalStorage = require('./localstorage-auth');
 const request = require('superagent');
-const BB = require('bluebird');
+let unauthorizedRoute = null;
 
-module.exports = function(unauthorizedRoute){
+module.exports = {
 
-    this.unauthorizedRoute = unauthorizedRoute;
+    configure(options) {
+        unauthorizedRoute = options.unauthorizedRoute;
+    },
 
-    var setHeaders = function  (request,headers,token){
-        request
-            .set('Accept', 'application/json')
-            .set('X-API-Key', token);
-        for(var header in headers){
-            request.set(header,headers[header]);
-        }
-        return request;
-    };
-    
-    this.get = function(apiDomain,url,headers,token){
+    get(options){
+        let apiDomain = options.apiDomain || '';
+        let url = options.url || '';
+        let headers = options.headers || {};
+        let token = options.token;
 
-        return new BB((resolve,reject) => {
-            var authToken = token ? token : (authLocalStorage.enabled ? authLocalStorage.authToken : null);
-            var req = request.get(`${apiDomain}${url}`);
-            setHeaders(req,headers,authToken)
-                .end((err,res)=>{
-                    if (err){
-                        // If request is unauthorized
-                        if (res.status === 403 && this.unauthorizedRoute && window){
-                            console.error('http.js API GET Request Unauthorized');
-                            window.location = this.unauthorizedRoute;
-                        } else {
-                            console.error('http.js API GET Request Error');
-                            reject(err);
-                        }
-                    } else {
-                        resolve(res);
-                    }
-                });
-        });
-    };
-    this.post = function(apiDomain,url,data,headers,token){
+        let authToken = token ? token : (authLocalStorage.enabled ? authLocalStorage.authToken : null);
+        return request
+            .get(`${apiDomain}${url}`)
+            .set(getHeaders(headers, authToken))
+            .catch(err=>{
+                // If request is unauthorized
+                if (err.status === 403 && unauthorizedRoute&& window){
+                    console.error('http.js API GET Request Unauthorized');
+                    window.location = this.unauthorizedRoute;
 
-        return new BB((resolve,reject) => {
-            var authToken = token ? token : (authLocalStorage.enabled ? authLocalStorage.authToken : null);
-            var req = request.post(`${apiDomain}${url}`);
-            setHeaders(req,headers,authToken)
-                .send(this.data)
-                .end((err,res)=>{
-                    if (err){
-                        if (res.status === 403 && this.unauthorizedRoute && window){
-                            console.error('http.js API POST Request Unauthorized');
-                            window.location = this.unauthorizedRoute;
-                        } else {
-                            console.error('http.js API POST Request Error');
-                            reject(err);
-                        }
-                    } else {
-                        resolve(res);
-                    }
-                });
-        });
+                }
+            });
+    },
+
+    post(options){
+        let apiDomain = options.apiDomain || '';
+        let url = options.url || '';
+        let data = options.data || {};
+        let headers = options.headers || {};
+        let token = options.token;
+        let authToken = token ? token : (authLocalStorage.enabled ? authLocalStorage.authToken : null);
+
+        return request
+            .post(`${apiDomain}${url}`)
+            .set(getHeaders(headers,authToken))
+            .send(data)
+            .catch(err=>{
+                if (err.status === 403 && unauthorizedRoute && window){
+                    console.error('http.js API POST Request Unauthorized');
+                    window.location = this.unauthorizedRoute;
+                }
+            });
 
 
-    };
-    this.put = function(apiDomain,url,data,headers,token){
+    },
 
-        return new BB((resolve,reject) => {
-            var authToken = token ? token : (authLocalStorage.enabled ? authLocalStorage.authToken : null);
-            var req = request.put(`${apiDomain}${url}`);
-            setHeaders(req,headers,authToken)
-                .send(data)
-                .end((err,res)=>{
-                    if (err){
-                        if (res.status === 403 && this.unauthorizedRoute && window){
-                            console.error('http.js API PUT Request Unauthorized');
-                            window.location = this.unauthorizedRoute;
-                        } else {
-                            console.error('http.js API PUT Request Error');
-                            reject(err);
-                        }
-                    } else {
-                        resolve(res);
-                    }
-                });
-        });
+    put(options){
+        let apiDomain = options.apiDomain || '';
+        let url = options.url || '';
+        let data = options.data || {};
+        let headers = options.headers || {};
+        let token = options.token;
 
-    };
-    this.delete = function(apiDomain,url,headers,token){
+        let authToken = token ? token : (authLocalStorage.enabled ? authLocalStorage.authToken : null);
+        return request
+            .put(`${apiDomain}${url}`)
+            .set(getHeaders(headers,authToken))
+            .send(data)
+            .catch(err=>{
+                if (err.status === 403 && unauthorizedRoute&& window){
+                    console.error('http.js API PUT Request Unauthorized');
+                    window.location = this.unauthorizedRoute;
+                }
+            });
 
-        return new BB((resolve,reject) => {
-            var authToken = token ? token : (authLocalStorage.enabled ? authLocalStorage.authToken : null);
-            var req = request.delete(`${apiDomain}${url}`);
-            setHeaders(req,headers,authToken)
-                .end((err,res)=>{
-                    if (err){
-                        if (res.status === 403 && this.unauthorizedRoute && window){
-                            console.error('http.js API DELETE Request Unauthorized');
-                            window.location = this.unauthorizedRoute;
-                        } else {
-                            console.error('http.js API DELETE Request Error');
-                            reject(err);
-                        }
-                    } else {
-                        resolve(res);
-                    }
-                });
-        });
+    },
 
-    };
-    
+    delete(options){
+        let apiDomain = options.apiDomain || '';
+        let url = options.url || '';
+        let headers = options.headers || {};
+        let token = options.token;
+
+        let authToken = token ? token : (authLocalStorage.enabled ? authLocalStorage.authToken : null);
+        return request.delete(`${apiDomain}${url}`)
+            .set(getHeaders(headers,authToken))
+            .catch(err=>{
+                if (err.status === 403 && unauthorizedRoute&& window){
+                    console.error('http.js API DELETE Request Unauthorized');
+                    window.location = this.unauthorizedRoute;
+                }
+            });
+
+    }
+
 };
+
+function getHeaders(headers,token){
+    return Object.assign({}, {
+        'Accept': 'application/json',
+        'X-API-KEY': token
+    }, headers);
+}
